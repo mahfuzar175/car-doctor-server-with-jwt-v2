1f646ed91bb363a0ec8c26bcaf92e5341cb781c2
mahfuzar175 <mahfuzar175@gmail.com>
2023-10-30 23:36:23 +0600

    first commit

---
+++ __jwt__token__.js
@@ -0,0 +1,13 @@
+/**
+ * ---------------------------
+ *        MAKE API SECURE
+ * --------------------------
+ * The person who should have
+ * 
+ * 
+ * concept:
+ * 1. assign two token for each person (access token, refresh token)
+ * 2. 
+ * 
+ * 
+ */
